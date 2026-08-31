531308cc048e422236bd6bc48b8118d1d739cfee
Ricardo Trujillo Valderrama <ricardotruval@Ricardos-MacBook-Air.local>
2026-08-31 08:29:24 -0600

    LOGICA CASI TERMINADA DE ROCK PAPER SCISSORS 0.1

--- Proyecto.Core/Juego.cs
+++ Proyecto.Core/Juego.cs
@@ -1,7 +1,43 @@
-﻿namespace Proyecto.Core;
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace Proyecto.Core;
 
 public class Juego
 {
+    int probabPiedra;
+    int probabPapel;
+    int probabTijera;
+    private List<Opcion> jugadasGuardadas = new List<Opcion>();
+
+    public void almacenarInformacion(Jugada opcionActual)
+    {
+        guardarJugada(opcionActual);
+    }
+    public void guardarJugada(Jugada opcionActual)
+    {
+        jugadasGuardadas.Add(opcionActual.OpcionElegida);
+    }
+
+    
+    public void probabilidadesTotales(List<Opcion> jugadasGuardadas)
+    {
+        foreach(Opcion jugada in jugadasGuardadas)
+        {
+            if(jugada == Opcion.Piedra){
+                probabPiedra++;
+            }
+            if(jugada == Opcion.Papel)
+            {
+                probabPapel++;
+            }
+            if(jugada == Opcion.Tijera)
+            {
+                probabTijera++;
+            }
+        }
+    }
+
+
     public void procesarEleccion(Jugada enJuego)
     {
         switch (enJuego.OpcionElegida)
@@ -20,11 +56,12 @@ public class Juego
 
             break;
         }
-
     }
 
-    public void CompararEleccion()
+    public void jugadaMaquina(List<Opcion> jugadasGuardadas, Jugada enJuego)
     {
-        
+        Random numRand = new Random();
+        int index = numRand.Next(0 , jugadasGuardadas.Count);
+        Opcion eleccionMaquina = jugadasGuardadas[index];
     }
 }
